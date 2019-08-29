be846f27b2a52bfa7fe163f8ee0e5baab6ce4a9b
Sameed Shah <sameedshah9@gmail.com>
2019-08-29 15:51:53 +0500

    Login Screen done

---
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.sameedshah_squadtech.babyguro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+    }
+}
--- app/src/main/java/com/sameedshah_squadtech/babyguro/MainActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sameedshah_squadtech.babyguro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
         );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCustomer = findViewById(R.id.btnCustomer);
+        Button btnVendor = findViewById(R.id.btnVendor);
+
+        btnCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+        btnVendor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
