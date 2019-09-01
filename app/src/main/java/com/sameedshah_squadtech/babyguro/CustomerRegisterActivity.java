a73c648e456c25e7d3d9806ff1ce2f15a0190ea0
Sameed Shah <sameedshah9@gmail.com>
2019-09-01 15:18:48 +0500

    register for Vendors done

--- app/src/main/java/com/sameedshah_squadtech/babyguro/CustomerRegisterActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/CustomerRegisterActivity.java
@@ -18,6 +18,7 @@ public class CustomerRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_register);
 
---
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/VendorRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.sameedshah_squadtech.babyguro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VendorRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_register);
+
+    }
+}
