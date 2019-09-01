6d79e8247a818f95facd8e1789da6da6b3c1bcb1
Sameed Shah <sameedshah9@gmail.com>
2019-09-01 14:09:38 +0500

    Login Screen done

---
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/CustomerRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.sameedshah_squadtech.babyguro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_register);
+    }
+}
--- app/src/main/java/com/sameedshah_squadtech/babyguro/LoginActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/LoginActivity.java
@@ -2,17 +2,38 @@ package com.sameedshah_squadtech.babyguro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnSignup = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Register Successful",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
