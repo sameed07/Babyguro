b7c92f19a0bc39e9548e07c043d93e963cad449a
Sameed Shah <sameedshah9@gmail.com>
2019-09-04 15:13:05 +0500

    drawable layout

--- app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
@@ -1,18 +1,25 @@
 package com.sameedshah_squadtech.babyguro;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        drawerLayout =   findViewById(R.id.drawer_layout);
         setToolbar();
+        configureNavigationDrawer();
     }
 
     private void setToolbar(){
@@ -22,5 +29,30 @@ public class HomeActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setHomeAsUpIndicator(R.drawable.ic_side_menu_24dp);
+    }
+    private void configureNavigationDrawer() {
+        NavigationView navView =  findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+//                f  = null;
+//                int itemId = menuItem.getItemId();
+//                if (itemId == R.id.action_home) {
+//                    f = new HomeFragment();
+//                } else if (itemId == R.id.action_cat) {
+//                    f = new CategoryFragment();
+//                }
+//                if (f != null) {
+//                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                    transaction.replace(R.id.frame, f);
+//                    transaction.commit();
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
+                return false;
+            }
+
+        });
     }
 }
