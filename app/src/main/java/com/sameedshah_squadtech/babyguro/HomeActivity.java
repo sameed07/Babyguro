968d0be8d2d6f147e503ddb96696adb927c2c0ee
Sameed Shah <sameedshah9@gmail.com>
2019-09-04 16:12:36 +0500

    drawable layout modifications

--- app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
@@ -2,24 +2,37 @@ package com.sameedshah_squadtech.babyguro;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         drawerLayout =   findViewById(R.id.drawer_layout);
-        setToolbar();
+        //changed from here
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         configureNavigationDrawer();
+        setToolbar();
+
     }
 
     private void setToolbar(){
@@ -52,7 +65,31 @@ public class HomeActivity extends AppCompatActivity {
 //                }
                 return false;
             }
-
         });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_cat) {
+           // Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+}
 }
