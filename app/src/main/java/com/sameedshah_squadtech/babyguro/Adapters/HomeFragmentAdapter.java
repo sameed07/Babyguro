095962bda67e07c7433218e828b929052e3fe6eb
Sameed Shah <sameedshah9@gmail.com>
2019-09-08 12:30:13 +0500

    adapter added to fragment

--- app/src/main/java/com/sameedshah_squadtech/babyguro/Adapters/HomeFragmentAdapter.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/Adapters/HomeFragmentAdapter.java
@@ -3,6 +3,7 @@ package com.sameedshah_squadtech.babyguro.Adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,36 +15,54 @@ import com.sameedshah_squadtech.babyguro.R;
 public class HomeFragmentAdapter extends  RecyclerView.Adapter<HomeFragmentAdapter.ViewHolder> {
 
         int[] images ;
-        String[] title ;
-        String[] price ;
-private Context mContext;
+
+
+        private Context mContext;
+
+        public HomeFragmentAdapter(Context context, int[] images){
+            this.mContext = context;
+            this.images = images;
+
+        }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.home_adapter,
+                viewGroup, false);
+        return new HomeFragmentAdapter.ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
+            viewHolder.mProdictImage.setImageResource(images[i]);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.length ;
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder  {
 
-        ImageView mImage;
-        TextView mTitle;
-        TextView mPrice;
+        ImageView mProdictImage,mCartButton;
+        TextView txtRating,txtProductName,txtVendorsName,txtPrice;
+
 
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            mProdictImage = itemView.findViewById(R.id.item_image);
+            mCartButton = itemView.findViewById(R.id.addToCartBtn);
+            txtProductName = itemView.findViewById(R.id.txt_product_name);
+            txtVendorsName = itemView.findViewById(R.id.txt_vendor_name);
+            txtRating = itemView.findViewById(R.id.txtRating);
+            txtPrice = itemView.findViewById(R.id.txt_Price_name);
 
         }
 
--- app/src/main/java/com/sameedshah_squadtech/babyguro/Fragments/HomeFragment.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/Fragments/HomeFragment.java
@@ -4,20 +4,37 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sameedshah_squadtech.babyguro.Adapters.HomeFragmentAdapter;
 import com.sameedshah_squadtech.babyguro.R;
 
 public class HomeFragment extends Fragment {
 
+
+    private RecyclerView mRecycler;
+    RecyclerView.LayoutManager manager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        int[] images = {R.drawable.asset_one,R.drawable.asset_one,R.drawable.asset_one
+                ,R.drawable.asset_one,R.drawable.asset_one,R.drawable.asset_one,
+                R.drawable.asset_one};
+
+        mRecycler = view.findViewById(R.id.home_recycler);
+        mRecycler.setLayoutManager(new GridLayoutManager(getActivity(),2));
+        HomeFragmentAdapter adapter = new HomeFragmentAdapter(getActivity(),images);
+        mRecycler.setAdapter(adapter);
+
 
         return view;
     }
