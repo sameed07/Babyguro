62b107d521e0c3721ae5c528178672e22672b1a6
Sameed Shah <sameedshah9@gmail.com>
2019-09-08 11:20:22 +0500

    Fragments thing done

---
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/DashboardFragment.java
@@ -0,0 +1,21 @@
+package com.sameedshah_squadtech.babyguro;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.dashboard_layout, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.sameedshah_squadtech.babyguro;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -21,20 +22,18 @@ public class HomeActivity extends AppCompatActivity {
 
     ActionBarDrawerToggle mToggle;
 
+    Fragment f;
+    HomeFragment hm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
          drawerLayout =   findViewById(R.id.drawer_layout);
-//        //changed from here
-//        mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close);
-//        drawerLayout.addDrawerListener(mToggle);
-//        mToggle.syncState();
-//
-//
-//        configureNavigationDrawer();
-//        setToolbar();
+        hm  = new HomeFragment();
+        replaceFragment(hm);
+        configureNavigationDrawer();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -77,4 +76,38 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
 }
+    private void configureNavigationDrawer() {
+        NavigationView navView =  findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                f  = null;
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.action_home) {
+                    f = new HomeFragment();
+                } else if (itemId == R.id.action_dashboard) {
+                    f = new DashboardFragment();
+                }
+                if (f != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, f);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    //fragment transition
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+
+    }
 }
