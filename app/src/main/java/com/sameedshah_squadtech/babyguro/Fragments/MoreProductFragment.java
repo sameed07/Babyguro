3bcafb25fc71335e43056b35b3b697359fc3600c
Sameed Shah <sameedshah9@gmail.com>
2019-09-18 12:18:14 +0500

    more product fragment done

--- app/src/main/java/com/sameedshah_squadtech/babyguro/Fragments/MoreProductFragment.java
+++ app/src/main/java/com/sameedshah_squadtech/babyguro/Fragments/MoreProductFragment.java
@@ -2,22 +2,34 @@ package com.sameedshah_squadtech.babyguro.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sameedshah_squadtech.babyguro.Adapters.HomeFragmentAdapter;
 import com.sameedshah_squadtech.babyguro.R;
 
 public class MoreProductFragment extends Fragment {
 
+    private RecyclerView mRecycler;
     public MoreProductFragment(){
 
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View PageTree = inflater.inflate(R.layout.more_product_fragment, container, false);
+        View view = inflater.inflate(R.layout.more_product_fragment, container, false);
 
 
+        mRecycler = view.findViewById(R.id.more_product_recycler);
+        int[] images = {R.drawable.asset_one,R.drawable.asset_one,R.drawable.asset_one
+                ,R.drawable.asset_one,R.drawable.asset_one,R.drawable.asset_one,
+                R.drawable.asset_one};
 
-        return PageTree;
+        mRecycler.setLayoutManager(new GridLayoutManager(getActivity(),2));
+        HomeFragmentAdapter adapter = new HomeFragmentAdapter(getContext(),images);
+        mRecycler.setAdapter(adapter);
+
+        return view;
     }
 }
